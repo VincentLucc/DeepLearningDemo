2ad7a16a6f4cc5cffa5e8e977bfb142268218fc6
Vincent Lu <vincent.cc.lu@gmail.com>
2025-05-07 14:21:49 -0400

    Renamed the project

--- Deep Learning Demo/Properties/AssemblyInfo.cs
+++ Deep Learning Demo/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Deep_Learning_Demo")]
+[assembly: AssemblyTitle("Deep_Learning_API")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Deep_Learning_Demo")]
+[assembly: AssemblyProduct("Deep_Learning_API")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
